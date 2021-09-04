8d48a3c09801151bb18b62800e351a556457c0ac
ksktada <ksk.tada@gmail.com>
2021-09-04 19:24:56 +0900

    fix but TLE

--- atcoder/abc/216/c.py
+++ atcoder/abc/216/c.py
@@ -1,15 +1,48 @@
 n = int(input())
 
 a = "A"
+b = 1
 
 if n == 1:
     print("A")
     exit()
 
-if n%2 != 0:
-    n = n - 1
+if n == 2:
+    print("AB")
+    exit()
 
-while(n != 1):
-    a = a + "B"
-    n = n / 2
+if n == 3:
+    print("AAA")
+    exit()
+
+if n == 4:
+    print("AAB")
+    exit()
+
+if n == 5:
+    print("AABA")
+    exit()
+
+is_even = True
+
+if n % 2 == 0:
+    n = int(n / 2)
+else:
+    n = int((n - 1) / 2)
+    is_even = False
+
+while(b != n):
+    b = b * 2
+
+    if b < n:
+        a = a + "B"
+    else:
+        b = int(b / 2)
+        c = n - b
+
+        for i in range(0, c):
+            a = a + "A"
+        break
+
+a = a + "B"
 print(a)
