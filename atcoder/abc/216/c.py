99ebd2d2b71602a5ba8f07c07f7257d591621c17
ksktada <ksk.tada@gmail.com>
2021-09-04 19:42:06 +0900

    fix but wrong answer

--- atcoder/abc/216/c.py
+++ atcoder/abc/216/c.py
@@ -1,48 +1,12 @@
 n = int(input())
 
-a = "A"
-b = 1
+a = ""
 
-if n == 1:
-    print("A")
-    exit()
-
-if n == 2:
-    print("AB")
-    exit()
-
-if n == 3:
-    print("AAA")
-    exit()
-
-if n == 4:
-    print("AAB")
-    exit()
-
-if n == 5:
-    print("AABA")
-    exit()
-
-is_even = True
-
-if n % 2 == 0:
-    n = int(n / 2)
-else:
-    n = int((n - 1) / 2)
-    is_even = False
-
-while(b != n):
-    b = b * 2
-
-    if b < n:
-        a = a + "B"
+while(n != 0):
+    if n % 2 == 0:
+        n = n / 2
+        a = "B" + a
     else:
-        b = int(b / 2)
-        c = n - b
-
-        for i in range(0, c):
-            a = a + "A"
-        break
-
-a = a + "B"
+        n = n - 1
+        a = "A" + a
 print(a)
